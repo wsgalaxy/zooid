fe6de273dbfdff50e44862099bf6e9c9d359f938
wsgalaxy <wsgalaxy@qq.com>
2022-01-26 10:55:09 +0800

    Support async task

--- examples/benchmark.rs
+++ examples/benchmark.rs
@@ -1,2 +1,122 @@
+use tokio::time::Instant;
+use zooid::{Actor, Addr, Context, ContextBuilder};
+
+struct Ping {
+    recorder: Addr<Recorder>,
+    pong: Addr<Pong>,
+}
+
+impl Actor for Ping {
+    type Msg = ();
+    type Rsp = ();
+
+    fn start(&mut self, _ctx: &mut Context<Self>, _addr: zooid::Addr<Self>) {
+        let recorder = self.recorder.clone();
+        let pong = self.pong.clone();
+        _ctx.spawn(async move {
+            let sleep = tokio::time::sleep(tokio::time::Duration::from_millis(200));
+            tokio::pin!(sleep);
+            let mut count: usize = 0;
+            loop {
+                tokio::select! {
+                    rsp = pong.send(()) => {
+                        if let Err(_) = rsp {
+                            println!("Peer dead!");
+                            break;
+                        } else {
+                            count += 1;
+                        }
+                    }
+
+                    _ = &mut sleep => {
+                        let _ = recorder.send(count).await;
+                        count = 0;
+                        sleep.as_mut().reset(Instant::now() + tokio::time::Duration::from_millis(200));
+                    }
+                }
+            }
+        }, |_, _, _| {});
+    }
+
+    fn stop(&mut self, _ctx: &mut Context<Self>) {
+        println!("Ping stopped");
+    }
+}
+
+struct Pong;
+
+impl Actor for Pong {
+    type Msg = ();
+    type Rsp = ();
+
+    fn handle_msg(&mut self, _ctx: &mut Context<Self>, _msg: Self::Msg) -> Option<Self::Rsp> {
+        Some(())
+    }
+
+    fn stop(&mut self, _ctx: &mut Context<Self>) {
+        println!("Pong stopped");
+    }
+}
+
+struct Recorder {
+    count: usize,
+}
+
+impl Recorder {
+    fn dump_statistics(_ctx: &mut Context<Self>, _act: &mut Self, _: ()) {
+        println!("{}/s", _act.count);
+        _act.count = 0;
+        _ctx.spawn(
+            async move {
+                tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+            },
+            Recorder::dump_statistics,
+        );
+    }
+}
+
+impl Actor for Recorder {
+    type Msg = usize;
+    type Rsp = ();
+
+    fn start(&mut self, _ctx: &mut Context<Self>, _addr: Addr<Self>) {
+        Recorder::dump_statistics(_ctx, self, ());
+    }
+
+    fn handle_msg(&mut self, _ctx: &mut Context<Self>, _msg: Self::Msg) -> Option<Self::Rsp> {
+        self.count += _msg;
+        None
+    }
+
+    fn stop(&mut self, _ctx: &mut Context<Self>) {
+        println!("Recorder sotpped");
+    }
+}
+
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    let mut args = std::env::args();
+    let mut cocurrency = num_cpus::get();
+
+    if args.len() >= 2 {
+        if let Ok(n) = args.nth(1).unwrap().parse() {
+            cocurrency = n;
+        }
+    }
+
+    println!("Use cocurrency {}", cocurrency);
+
+    let addr_recorder = ContextBuilder::new(1024).start(Recorder { count: 0 });
+    let mut addr_ping_vec = Vec::new();
+
+    for _ in 0..cocurrency {
+        addr_ping_vec.push(ContextBuilder::new(1024).start(Ping {
+            recorder: addr_recorder.clone(),
+            pong: ContextBuilder::new(1024).start(Pong),
+        }));
+    }
+
+    loop {
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::marker::PhantomData;
+use std::collections::HashMap;
+use std::{future::Future, marker::PhantomData};
 use tokio::sync::{mpsc, oneshot};
 
 pub struct SendError<T>(T);
@@ -18,6 +19,7 @@ impl<A> Addr<A>
 where
     A: Actor,
 {
+    /// Send message to Actor and wait for response.
     pub async fn send(&self, msg: A::Msg) -> Result<Option<A::Rsp>, SendError<A::Msg>> {
         let (tx, rx) = oneshot::channel();
         // Send to actor
@@ -37,9 +39,18 @@ where
             Err(_) => Ok(None),
         }
     }
-    pub fn try_send() -> Result<(), TrySendError<A::Rsp>> {
-        // TODO
-        Ok(())
+
+    /// Try to send a message to Actor and not wait for the response, fail if the
+    /// channel is full or the actor is stopped.
+    pub fn try_send(&self, msg: A::Msg) -> Result<(), TrySendError<A::Msg>> {
+        if let Err(e) = self.tx.try_send(Envelope { msg, rsp_tx: None }) {
+            match e {
+                mpsc::error::TrySendError::Full(evlp) => Err(TrySendError::Full(evlp.msg)),
+                mpsc::error::TrySendError::Closed(evlp) => Err(TrySendError::Closed(evlp.msg)),
+            }
+        } else {
+            Ok(())
+        }
     }
 }
 
@@ -54,8 +65,8 @@ where
     }
 }
 
-pub trait Message: Send + Sized + 'static {}
-pub trait Response: Send + Sized + 'static {}
+pub trait Message: Send + 'static {}
+pub trait Response: Send + 'static {}
 
 impl<T> Message for T where T: Send + Sized + 'static {}
 impl<T> Response for T where T: Send + Sized + 'static {}
@@ -73,6 +84,9 @@ pub struct Context<A>
 where
     A: Actor,
 {
+    pack_tx: mpsc::Sender<Box<dyn Package<A>>>,
+    pack_rx: Option<mpsc::Receiver<Box<dyn Package<A>>>>,
+    task_handles: HashMap<usize, Box<dyn AsyncTaskHandle>>,
     _p1: PhantomData<A>,
 }
 
@@ -81,7 +95,12 @@ where
     A: Actor,
 {
     fn new() -> Self {
+        // TODO: Support config channel size
+        let (pack_tx, pack_rx) = mpsc::channel(64);
         Context {
+            pack_tx,
+            pack_rx: Some(pack_rx),
+            task_handles: HashMap::new(),
             _p1: Default::default(),
         }
     }
@@ -93,15 +112,72 @@ where
         mut rx: mpsc::Receiver<Envelope<A::Msg, A::Rsp>>,
     ) {
         act.start(&mut self, addr);
+        let mut pack_rx = self.pack_rx.take().unwrap();
+        let mut pack_rx_closed = false;
         loop {
-            match rx.recv().await {
-                Some(evlp) => {
-                    act.handle_msg_async(&mut self, evlp.msg, evlp.rsp_tx);
+            tokio::select! {
+                // Receive message from Addr and make the message be handled by actor
+                evlp = rx.recv() => {
+                    if let Some(evlp) = evlp {
+                        act.handle_msg_async(&mut self, evlp.msg, evlp.rsp_tx);
+                    } else {
+                        // All Addr dropped, this actor is going to die.
+                        break;
+                    }
                 }
-                None => break,
+                // Receive package and unpack it
+                pack = pack_rx.recv(), if !pack_rx_closed => {
+                    if let Some(mut pack) = pack {
+                        pack.unpack(&mut self, &mut act);
+                    } else {
+                        pack_rx_closed = true;
+                    }
+                }
             }
         }
+        // Cancel all async tasks before we stop
+        self.clear_async_tasks();
+        // Now stop Actor
         act.stop(&mut self);
+    }
+
+    pub fn spawn<Fut, F>(&mut self, fut: Fut, f: F)
+    where
+        Fut: Future + Send + 'static,
+        Fut::Output: Send + 'static,
+        F: FnOnce(&mut Context<A>, &mut A, <Fut as Future>::Output) + Send + 'static,
+    {
+        // Request an id and if failed, just panic
+        let id = self.gen_id();
+        let pack_tx = self.pack_tx.clone();
+        let jh = tokio::spawn(async move {
+            let _ = pack_tx
+                .send(Box::new(FuturePackage {
+                    fut_res: Some(fut.await),
+                    callback: Some(f),
+                    id,
+                    _p1: Default::default(),
+                }))
+                .await;
+        });
+        // Register this async task so we get the chance to cancel it
+        self.task_handles
+            .insert(id, Box::new(FutureHandle { jh: Some(jh) }));
+    }
+
+    fn clear_async_tasks(&mut self) {
+        for (_, v) in &mut self.task_handles {
+            v.cancel();
+        }
+        self.task_handles.clear();
+    }
+
+    fn gen_id(&mut self) -> usize {
+        let mut id = rand::random();
+        while self.task_handles.contains_key(&id) {
+            id = rand::random();
+        }
+        id
     }
 }
 
@@ -162,5 +238,60 @@ pub trait Actor: Send + Sized + 'static {
         if let Some(tx) = rsp_tx {
             let _ = tx.send(rsp.unwrap());
         }
+    }
+}
+
+trait Package<A>: Send + 'static
+where
+    A: Actor,
+{
+    fn unpack(&mut self, ctx: &mut Context<A>, act: &mut A);
+}
+
+struct FuturePackage<A, FutRes, F>
+where
+    A: Actor,
+    FutRes: Send + 'static,
+    F: FnOnce(&mut Context<A>, &mut A, FutRes) + Send + 'static,
+{
+    fut_res: Option<FutRes>,
+    callback: Option<F>,
+    id: usize,
+    _p1: PhantomData<A>,
+}
+
+impl<A, FutRes, F> Package<A> for FuturePackage<A, FutRes, F>
+where
+    A: Actor,
+    FutRes: Send + 'static,
+    F: FnOnce(&mut Context<A>, &mut A, FutRes) + Send + 'static,
+{
+    fn unpack(&mut self, ctx: &mut Context<A>, act: &mut A) {
+        // Remove JoinHandle
+        ctx.task_handles.remove(&self.id);
+
+        let callback = self.callback.take().unwrap();
+        let fut_res = self.fut_res.take().unwrap();
+        callback(ctx, act, fut_res);
+    }
+}
+
+trait AsyncTaskHandle: Send + 'static {
+    fn cancel(&mut self);
+}
+
+struct FutureHandle<T>
+where
+    T: Send + 'static,
+{
+    jh: Option<tokio::task::JoinHandle<T>>,
+}
+
+impl<T> AsyncTaskHandle for FutureHandle<T>
+where
+    T: Send + 'static,
+{
+    fn cancel(&mut self) {
+        self.jh.take().unwrap().abort();
     }
 }
